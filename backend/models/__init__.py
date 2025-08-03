57df0c7bc6e0aa52153fc69b2ad4b868ccb8c843
주재현 <jaehyun.joo@yplabs.kr>
2025-08-03 19:00:09 +0900

    chore: model all 추가

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,4 +1,6 @@
+from sqlmodel import SQLModel
+
 from .blog import Blog, BlogPost
 from .models import Item, User
 
-__all__ = ["Blog", "BlogPost", "User", "Item"]
+__all__ = ["Blog", "BlogPost", "User", "Item", "SQLModel"]
